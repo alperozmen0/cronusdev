57be8a8991bc0ed8b45a974de429cf090495922c
alperozmen0 <121893378+alperozmen0@users.noreply.github.com>
2023-10-19 21:21:55 +0300

    Update About.tsx

--- components/home/about/About.tsx
+++ components/home/about/About.tsx
@@ -11,7 +11,7 @@ export const About = () => {
         <div>
           <Reveal>
             <p className={`${styles.aboutText}`}>
-              <span>Hi</span> there! My name is Shiva Bhattacharjee, and I&apos;m a Computer Science student with a passion for creating and designing websites. I have two years of experience in web development, specializing in the MERN stack. I enjoy coding and strive to build modern and visually appealing websites that provide excellent user experiences. I am proficient in front-end technologies such as HTML, CSS, JavaScript, and React, as well as back-end technologies like Node.js and MongoDB. I am always eager to learn and explore new technologies and frameworks. If you&apos;re looking for a developer who can deliver high-quality websites with a creative touch, I&apos;m here to help. Let&apos;s collaborate and bring your vision to life!
+              <span>Ulan!</span> sanal magandayım , ifşalanmam , yıkılmam , ezilmem , bükülmem , sarsılmam , kayıtlara geçmiş 57 tane hakaret , 29 tane adam yralama , 9 tane ciayet davam var , ileri derecede bipolarım istediğim kişiyi kolay bir şekilde in***ara sürüklerim, kimse baş edemez , sanalın tek mafyası benim , sevdiğimi korurum , laf atanı ifşalarım , ailesine haciz getirtirim , baba parası yemem kendi paramı alın teriyle kazanır parayı karımla çatur çutur yerim, sicilim çok kabarık , 3 yıldız polisler tarafından aranıyorum , aşırı derecede karizmayım , insan anlamam , birinci vazifem kadınlardır , ne mutlu türkiyem sanal maganda
             </p>
           </Reveal>
         </div>
